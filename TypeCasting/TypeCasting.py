c834e8561cf45bb87b7a872a8189c94ea745cfa2
Anamul Haque <anamulhaque4631@gmail.com>
2026-08-22 00:14:29 +0600

    operator, type csting , conditional

---
+++ TypeCasting/TypeCasting.py
@@ -0,0 +1,20 @@
+# type casting in python
+
+# number = float , integer
+
+# age = int(input("tou"))
+# weight = float(input("Weight : "))
+
+# if age >= 18:
+#     print("You are adult")
+# else:
+#     print("you are child")
+
+# if weight >= 60:
+#     print("You are fat")
+# else:
+#     print("you are slim")
+age=15
+weight =15
+print(int(weight))
+print(float(age))

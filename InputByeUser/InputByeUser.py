1b42147ef810ce515219d5bbb80c8cddea2e69aa
Anamul Haque <anamulhaque4631@gmail.com>
2026-08-21 20:37:29 +0600

    Start learn python

---
+++ InputByeUser/InputByeUser.py
@@ -0,0 +1,7 @@
+# Input bye user
+
+userName =input("Enter your name : ")
+userAge = input("Enter your age : ")
+print(  "Hello",userName, "My age is" , userAge)
+
+# input() ==== always store string format data
---
+++ variable/variable.py
@@ -0,0 +1,24 @@
+# Variable are containers for sorting data values , like number and characters
+# variable is treated as a name or Identifier that indicates thw memory location where it Store the value
+
+# type of variable
+"""
+*1. number, 
+*2. string, 
+*3.boolean, 
+*4. null
+*5. list type variable
+
+
+"""
+_= 5 # special variable declare 
+
+
+box ="valentine ring" #string
+
+plate ="food"  #string
+
+num1 = 10 #number integer
+num2 = 1.5 # float number
+market_list =["potato","sos",2,1.2]
+print(num1,market_list)

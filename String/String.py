9e74a4de62d99e6e299da3ca85202d3721d8833e
Anamul Haque <anamulhaque4631@gmail.com>
2026-09-06 22:34:21 +0600

    String type and string method

---
+++ String/String.py
@@ -0,0 +1,48 @@
+#  introduction to string in python
+
+# declare string
+name = " Hello Rahim"
+string = "anamul"
+multipleLine = """ this is rohim
+anamul ,
+muliple line"""
+
+# name[0]="W" # TypeError: 'str' object does not support item assignment
+# print(name[0])
+
+# print(name[-1])
+
+# for i in name:
+#     print(i, end=" ")
+
+# print(name[2:10:3])
+
+
+a = "Hello WorlD Python"
+
+# lower case : lower()
+# print(a.lower())
+
+# upper case : upper()
+
+# print(a.upper())
+
+# convert string to title case
+# print(a.title())
+
+# isupper()
+
+# print(a.isupper())
+
+# islower()
+# print(a.islower())
+
+
+# istitle()
+
+# print("Hello".istitle())
+# print(a.istitle())
+
+# isalpha(): check alphabet
+x = "Hello123"
+# print(x.isalpha())
---
+++ String/StringMethod.py
@@ -0,0 +1,44 @@
+a = "hello world python"
+
+# capitalize()
+
+# print(a.capitalize())
+
+# title()
+
+# print(a.title())
+
+
+# swapcase()
+# print(a.swapcase()) #small later change capital and capital small
+
+
+# casefold() text ="grob"
+
+# print(a.casefold()) # more powerfull
+
+# replace()
+b = "helle"
+c = b.replace("e", "o", 2)
+# print(c)
+
+# # count()
+# print(a.count("H"))
+
+
+# isDigit()
+age = "123"
+# print(age.isdigit())
+
+# join()
+
+listString = ["h", "e", "l", "l", "o"]
+listNumber = ["1", "2", "3", "4", "5", "6"]
+# print("".join(listNumber))
+# print("".join(listString))
+# split()
+c = "Hello"
+d = list(c)
+
+
+print(c.split())
